95f72bcdd0cd04620035208ee90fd79ff397a9ae
unknown <denzdemetillar@gmail.com>
2023-05-20 14:36:03 +0800

    first commit

--- data_struct_binary_tree.c
+++ data_struct_binary_tree.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int InputChoice(); 
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+int InputChoice();
+int InputData();  
 void DisplayMenu(); 
-void GoToChoice();
+void GoToChoice(int choice, struct Node **root);
+void PrintNodes(struct Node *root);
+int IsFullBinaryTree(struct Node *root);
+struct Node *CreateNode(int data);
+struct Node *InsertNode(struct Node *root, int data);
 
 int main()
 {
+	struct Node *root = NULL;
 	int choice = 0;
 	
 	do{  
         DisplayMenu();
         choice = InputChoice();
-        GoToChoice(choice);
+        GoToChoice(choice, &root);
     }while(choice!=6);
     
     return 0;
@@ -24,6 +36,15 @@ int InputChoice() {
     fflush(stdin);
 
     return choice;
+}
+
+int InputData() {
+	int data;
+	
+	printf("Enter the value to insert: ");
+ 	scanf("%d", &data);
+	
+	return data;
 } 
 
 void DisplayMenu()
@@ -37,29 +58,89 @@ void DisplayMenu()
     printf("\nChoice: "); 
 }
 
-void GoToChoice(int choice) {
-    int i;
+void GoToChoice(int choice, struct Node **root) {
+    int i, data, isFull;
 	switch (choice) {
         case 1:
-            
-            break;
+			data = InputData();
+			*root = InsertNode(*root, data);
+		break;
         case 2:
             
-            break;
+        break;
         case 3:
             
-            break;
+        break;
         case 4:
             
-            break;
-         case 5:
-            
-            break;
-        case 6:
+        break;
+       	case 5:
+            PrintNodes(*root);
+        break;
+  		case 6:
             printf("Exiting...\n");
-            break;
+    	break;
         default:
             printf("\nChoice Invalid.\n");
-            break;
+        break;
     }
+}
+
+struct Node *CreateNode(int data) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node *InsertNode(struct Node *root, int data) {
+    if (root == NULL){
+    	return CreateNode(data);
+    }
+    
+    struct Node* currentNode = root;
+    struct Node* parentNode = NULL;
+
+    while (currentNode != NULL) {
+        parentNode = currentNode;
+        if (data <= currentNode->data){
+        	currentNode = currentNode->left;
+        }
+        else{
+        	currentNode = currentNode->right;
+        }
+    }
+
+    if (data <= parentNode->data){
+    	parentNode->left = CreateNode(data);
+    }
+    else{
+    	parentNode->right = CreateNode(data);
+    }
+
+    return root;
+}
+
+int IsFullBinaryTree(struct Node *root) {
+    if (root == NULL)
+        return 1;
+    
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+    
+    if ((root->left != NULL && root->right != NULL) && 
+        IsFullBinaryTree(root->left) && IsFullBinaryTree(root->right))
+        return 1;
+    
+    return 0;
+}
+
+void PrintNodes(struct Node *root) {
+    if (root == NULL)
+        return;
+
+    printf("%d ", root->data);
+    PrintNodes(root->left);
+    PrintNodes(root->right);
 }
